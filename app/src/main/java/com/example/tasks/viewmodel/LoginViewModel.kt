bb203be6e2e1966f482250168862db76ddbae394
LuisTaborda <luisgustavo.taborda@gmail.com>
2020-09-22 21:45:37 -0300

    implementando login

--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,6 +2,8 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.repository.PersonRepository
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
@@ -12,7 +14,15 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
      * Faz login usando API
      */
     fun doLogin(email: String, password: String) {
-        mPersonRepository.login(email, password)
+        mPersonRepository.login(email, password, object: APIListener{
+            override fun onSuccess(model: HeaderModel) {
+
+            }
+
+            override fun onFailure(str: String) {
+
+            }
+        })
     }
 
     /**

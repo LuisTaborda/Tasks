aa94de87c7103c540a6329c9487a41442a3f074e
LuisTaborda <luisgustavo.taborda@gmail.com>
2020-09-24 21:01:08 -0300

    implementando login

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,9 +1,11 @@
 package com.example.tasks.service.repository
 
 import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.repository.remote.PersonService
 import com.example.tasks.service.repository.remote.RetrofitClient
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,17 +14,23 @@ class PersonRepository {
 
     private val mRemote = RetrofitClient.createService(PersonService::class.java)
 
-    fun login(email: String, password: String, listener : APIListener) {
-        val call: Call<HeaderModel> = mRemote.login(email,password)
+    fun login(email: String, password: String, listener: APIListener) {
+        val call: Call<HeaderModel> = mRemote.login(email, password)
         // Assincrona
-        call.enqueue(object : Callback<HeaderModel>{
+        call.enqueue(object : Callback<HeaderModel> {
             override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
                 listener.onFailure(t.message.toString())
             }
 
             override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
-                response.body()?.let { listener.onSuccess(it) }
-           }
+                if (response.code() != TaskConstants.HTTP.SUCCESS) {
+                    val validation =
+                        Gson().fromJson(response.errorBody()!!.string(), String::class.java)
+                    listener.onFailure(validation)
+                } else {
+                    response.body()?.let { listener.onSuccess(it) }
+                }
+            }
         })
     }
 }
